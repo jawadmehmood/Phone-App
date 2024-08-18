a1a8b6f4649c2a58f79a28be03a8214d65e5e924
jawadmehmood <jawadam5678@gmail.com>
2024-08-18 20:13:46 +0500

    Project Initialized

---
+++ app.js
@@ -0,0 +1,84 @@
+const phoneDetails = [
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A73',
+        ram: 8,
+        rom: 256,
+        camera: '108MP',
+        price: 170500
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A54',
+        ram: 8,
+        rom: 256,
+        camera: '50MP',
+        price: 140000
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A55',
+        ram: 8,
+        rom: 256,
+        camera: '50MP',
+        price: 140000
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A53',
+        ram: 8,
+        rom: 128,
+        camera: '64MP',
+        price: 134000
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A35',
+        ram: 8,
+        rom: 256,
+        camera: '50MP',
+        price: 120000
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A34',
+        ram: 8,
+        rom: 128,
+        camera: '48MP',
+        price: 115000
+    },
+    {
+        brand: 'Samsung',
+        model: 'Gakaxy A25',
+        ram: 8,
+        rom: 256,
+        camera: '50MP',
+        price: 99000
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy A72',
+        ram: 8,
+        rom: 128,
+        camera: '64MP',
+        price: 83000
+    }
+]
+
+const products = document.getElementById('products');
+
+for(let i = 0; i < phoneDetails.length; i++){
+    products.innerHTML += `
+    <div class="card" style="width: 18rem;">
+            <div class="card-body bg-dark text-light border border-light-subtle border-1 rounded">
+              <h5 class="card-title">${phoneDetails[i].brand} ${phoneDetails[i].model}</h5>
+              <h6 class="card-subtitle mt-2">Rs. ${phoneDetails[i].price}</h6>
+              <button class="btn btn-primary mt-4">Add to Cart</button>
+            </div>
+        </div>
+    `
+}
+
+function addToCart(){
+    
+}
